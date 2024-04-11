045a144cd19a85d326efecd9dfa918e7f4e8ba08
MatthieuGrauleau <m.grauleau@gmail.com>
2024-04-12 00:44:29 +0200

    changing if position

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -69,14 +69,15 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	}
 
-	if (!binary_tree_is_full(tree))
-	{
-		return (0);
-	}
-
 	if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
 	{
 		return (0);
 	}
+
+	if (!binary_tree_is_full(tree))
+	{
+		return (0);
+	}
+		
 	return (1);
 }
